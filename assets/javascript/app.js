c1898bb4d57177c7c3d8cea63819af230c9818c7
Alexander Bly <alexanderbly@gmail.com>
2018-07-16 17:41:43 -0700

    minor changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-//80s shows for gifs
-
 var topics = [
 "alf",
 "the a-team",
@@ -13,21 +11,17 @@ var topics = [
 "ghostbusters"];
 
 function displayGifs(){
-//searches giphy api for topic in search bar
     var topic = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uAplEeI1pMEKTa6F4y4V7UbniqfCRnOK&q="+ topic +"&limit=10&offset=0&lang=en";
 
-//ajax call for giphy
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
     console.log(response)
-    //creates gifs from choices supplied by giphy
     
     for (var i = 0; i < response.data.length; i++){
 
-//tried showDiv and #gif-area with same results. 
 var showDiv = $("<div class='show'>");
 
 var rating = response.data[i].rating;
@@ -51,7 +45,6 @@ $("#gif-area").append(imgGif);
 
 
 
-//play gifs by swapping static image for gif
 $("#gif-area").on('click', '.gif', function() {
     var src = $(this).attr("src");
     console.log(this);
@@ -70,12 +63,9 @@ $("#gif-area").empty();
 
 };
 
-//function to make buttons based on topics
 function renderButtons() {
 
-    //deletes buttons before adding new ones
     $("#button-area").empty();
-    //loops through topics array
     for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
         a.addClass("topic-btn");
@@ -85,15 +75,11 @@ function renderButtons() {
     }
 }
 
-//search function to add to topics
 
 $("#add-show").on("click", function(event) {
-    //prevents webpage from reloading on enter
     event.preventDefault();
-    //gets keywords from text-box
     var show = $("#show-input").val().trim();
     console.log(show);
-    //adds new shows to array
     topics.push(show);
     renderButtons();
 });
@@ -101,9 +87,3 @@ $("#add-show").on("click", function(event) {
 $(document).on("click", ".topic-btn", displayGifs);
 
 renderButtons();
-
-
-//function to display 10 gifs related to topic
-
-//ability to start and stop playback on click
-
